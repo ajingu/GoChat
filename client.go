80420151fe37fa5a6f9dbd08ce68809be8a81e8b
Arata Jingu <>
2018-08-11 13:01:09 +0900

    Implement  authentication function

--- client.go
+++ client.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gorilla/websocket"
 )
 
 type client struct {
-	socket *websocket.Conn
-	send chan [] byte
-	room *room
+	socket   *websocket.Conn
+	send     chan *message
+	room     *room
+	userData map[string]interface{}
 }
 
-func(c *client) read(){
-	for{
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
+func (c *client) read() {
+	for {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
 			break
@@ -21,9 +27,9 @@ func(c *client) read(){
 	c.socket.Close()
 }
 
-func(c *client) write(){
-	for msg := range c.send{
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil{
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
 	}
